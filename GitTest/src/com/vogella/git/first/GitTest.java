bac3f919680b76e1e99676314e2a97c7ac2dec87
cpjjpeters <cpjjpeters@gmail.com>
2019-03-21 07:31:08 +0100

    first commit

---
+++ GitTest/src/com/vogella/git/first/GitTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.vogella.git.first;
+
+/**
+ * @author cpjjpeters
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun!  (but there is a learning curve)");
+
+	}
+
+}
